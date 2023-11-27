b3a6994323815dc7ddba55fd940e21f0ac60d509
babi704-1 <134932539+babi704-1@users.noreply.github.com>
2023-11-27 09:07:32 -0300

    Add files via upload

---
+++ aluraMID-main/main.js
@@ -0,0 +1,10 @@
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play()
+}
+
+document.querySelectorAll('.tecla');
+
+const ListaDeTeclas
+
+
